d085c73aea403c622dcdc4fc035a6a2eb453de19
CryptexFxtrade-io <ikennachidiprince@gmail.com>
2025-12-18 13:06:26 +0100

    Update Plan.js

--- backend/models/Plan.js
+++ backend/models/Plan.js
@@ -2,9 +2,9 @@ import mongoose from "mongoose";
 
 const PlanSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  duration: { type: Number, required: true }, // in days
+  duration: { type: Number, required: true },
   profitPercent: { type: Number, required: true },
 });
 
 const Plan = mongoose.model("Plan", PlanSchema);
-export default Plan; // ✅ Default export
+export default Plan;
