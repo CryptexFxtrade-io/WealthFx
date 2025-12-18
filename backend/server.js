c9603bf71dea09e0d4c58cf63864076644536ce8
CryptexFxtrade-io <ikennachidiprince@gmail.com>
2025-12-18 09:54:28 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,1 @@
-const authRoutes = require("./routes/auth");
-const userRoutes = require("./routes/user");
-const adminRoutes = require("./routes/admin");
-
-app.use("/api/auth", authRoutes);
-app.use("/api/user", userRoutes);
-app.use("/api/admin", adminRoutes);
+require("./cron/roiJob");
