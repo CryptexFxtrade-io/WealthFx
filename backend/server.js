1aec11396668616b9bb0a71b1d6b60ae9795fff6
CryptexFxtrade-io <ikennachidiprince@gmail.com>
2025-12-18 09:41:45 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,7 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const cors = require("cors");
-require("dotenv").config();
-
 const authRoutes = require("./routes/auth");
-
-const app = express();
-app.use(cors());
-app.use(express.json());
-
-mongoose.connect(process.env.MONGO_URI)
-.then(() => console.log("✅ MongoDB Connected"))
-.catch(err => console.log(err));
+const userRoutes = require("./routes/user");
+const adminRoutes = require("./routes/admin");
 
 app.use("/api/auth", authRoutes);
-
-app.get("/", (req, res) => {
-  res.json({ message: "FxWealth API Running" });
-});
-
-app.listen(process.env.PORT, () =>
-  console.log("🚀 Server running on", process.env.PORT)
-);
+app.use("/api/user", userRoutes);
+app.use("/api/admin", adminRoutes);
